e6d3f3d24bb0a9b97b575a51d2a2e2b6bd7e4c6f
1cbyc <isaacnsisong@gmail.com>
2025-09-10 00:26:09 +0100

    added the connection logic

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,15 @@
 import mongoose from 'mongoose';
 
-export const connectDatabse = async () => {
-    // will add my implementation shortly
+export const connectDatabase = async () => {
+    try {
+        const mongoUri = process.env.MONGODB_URI;
+        if (!mongoUri) {
+            throw new Error('MONGODB_URI is not defined in the environment variables');
+        }
+        await mongoose.connect(mongoUri);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1); // so it would exit the app if db connection fails
+    }
 }
